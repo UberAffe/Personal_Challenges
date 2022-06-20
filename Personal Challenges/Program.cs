58ee0b485323a225cea3a5fe7ec067f2adf7598f
Matt Keran <mattkeran@gmail.com>
2022-06-20 10:51:31 -0500

    Initial commit

---
+++ Personal Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Personal_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
